5a6271fd13a63710af2b27dbb4f756c27c79568f
meyhem <meyhem@protonmail.com>
2021-10-31 20:20:20 +0100

    Case sensitivity

---
+++ Skid/Io.fs
@@ -0,0 +1,34 @@
+﻿module Skid.Io
+
+open System.IO
+open System.Text.RegularExpressions
+
+let resolvePath path = Path.GetFullPath path
+let pathExists path = FileInfo(path).Exists
+
+let isDir path =
+    (File.GetAttributes(path)
+     &&& FileAttributes.Directory) = FileAttributes.Directory
+
+let isFile path = path |> isDir |> not
+let loadFile path = File.ReadAllText path
+let writeFile path content = File.WriteAllText(path, content)
+let trimExtension (path: string) = Regex("\.skid$", RegexOptions.IgnoreCase).Replace(path, "")
+
+let listSkidFiles path recursive =
+    Directory.EnumerateFiles(
+        path,
+        "*.skid",
+        if recursive then
+            SearchOption.AllDirectories
+        else
+            SearchOption.TopDirectoryOnly
+    )
+    |> Seq.map resolvePath
+    |> Seq.toArray
+
+let discoverSkidFiles path recursive =
+    if isFile path then
+        [| resolvePath path |]
+    else
+        listSkidFiles path recursive
---
+++ Skid/Program.fs
@@ -0,0 +1,49 @@
+let die msg =
+    printf $"Error: {msg}"
+    exit 1
+
+[<EntryPoint>]
+let main argv =
+    let config =
+        Skid.Cli.getApplicationConfiguration argv
+
+//    let x =
+//        [| "--file"
+//               "D:/dev/skidtest/base.json"
+//               "--file"
+//               "D:/dev/skidtest/override.json"
+//               "--recursive"
+//               "D:/dev/skidtest/config.toml.skid" |]
+
+    for path in config.ValueFiles do
+        if not (Skid.Io.pathExists path) then
+            die $"Value file '{path}' does not exist"
+
+    let valueDocuments =
+        config.ValueFiles
+        |> Array.map Skid.Io.loadFile
+        |> Array.map Skid.Json.parse
+
+    let skidFiles =
+        Skid.Io.discoverSkidFiles config.Target config.Recursive
+
+    for skidFile in skidFiles do
+        let content = Skid.Io.loadFile skidFile
+        let marks = Skid.Templating.findAllMarks content
+
+        let values =
+            marks
+            |> List.map (fun mark -> mark.JsonPath, Skid.Json.selectMergedProp mark.JsonPath valueDocuments)
+            |> List.choose
+                (fun (path, optValue) ->
+                    match optValue with
+                    | Some v -> Some(path, v)
+                    | None -> None)
+            |> dict
+
+        printfn $"Templating {skidFile} ({List.length marks} marks)"
+
+        Skid.Templating.replaceAllMarks skidFile content marks values
+        |> Skid.Io.writeFile (Skid.Io.trimExtension skidFile)
+
+    0
