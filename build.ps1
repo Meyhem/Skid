03470cddc590520c78e4b2d1587945268eb5924b
meyhem <meyhem@protonmail.com>
2021-10-31 20:15:52 +0100

    ps fix

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ $artifact_folder = "artifacts"
 Remove-Item -Force -Recurse -ErrorAction Ignore -Path $artifact_folder
 New-Item -ItemType Directory -Force $artifact_folder
 
-$rids | foreach {
+$rids | ForEach-Object {
     Write-Host "Building $artifact_folder/package-$_"
     dotnet publish --nologo --output "$artifact_folder/package-$_" --configuration Release -r $_ -p:DebugType=None -p:PublishSingleFile=true -p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true .\Skid\Skid.fsproj
     
